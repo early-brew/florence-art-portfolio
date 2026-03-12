88f12fd10953841a69eb47788c4de34deea30591
florence <florence.gesine24@gmail.com>
2026-03-12 16:24:09 -0600

    up

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = (env, argv) => {
 
   return {
     mode: isDev ? "development" : "production",
-    entry: "./src/index.js",
+    entry: "./src/index.jsx",
     output: {
       path: path.resolve(__dirname, "dist"),
       filename: isDev ? "bundle.js" : "bundle.[contenthash].js",
